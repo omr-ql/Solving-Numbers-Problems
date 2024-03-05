1897851ff49ade6bd7d7b42b3c88a7dc5bcdb9db
Omar Abdullah <121067316+omr-ql@users.noreply.github.com>
2024-03-05 15:45:32 +0200

    Add files via upload

---
+++ calc the days between the dates.py
@@ -0,0 +1,7 @@
+d = int(input("Enter The First Day : "))
+m = int(input("Enter The First Month : "))
+y = int(input("Enter The First year : "))
+dd = int(input("Enter The Second Day : "))
+mm = int(input("Enter The Second Day : "))
+yy = int(input("Enter The Second Day : "))
+print(abs(dd-d),"Day",abs(mm-m),"Month",abs(yy-y),"Year")
